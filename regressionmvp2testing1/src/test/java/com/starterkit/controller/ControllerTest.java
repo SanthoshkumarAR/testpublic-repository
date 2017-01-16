e98cf29fdd3ef89427236770b6131ba32cc04aec
SanthoshkumarAR <SanthoshkumarAR>
2017-01-16 11:19:35 +0000

    Initial check-in for Reference Template code

---
+++ regressionmvp2testing1/src/test/java/com/starterkit/controller/ControllerTest.java
@@ -0,0 +1,11 @@
+package com.starterkit.controller;
+
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import com.starterkit.Application;
+
+/**
+ * @author narendra.gurram@cognizant.com
+ */
+@SpringApplicationConfiguration(classes = Application.class)
+public class ControllerTest {
+}
